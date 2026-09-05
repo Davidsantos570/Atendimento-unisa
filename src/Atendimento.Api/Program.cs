fc941f8dacf398c292210508f38cdc6ee26d3af7
david_santos <davidzks14@gmail.com>
2026-09-05 13:17:49 -0300

    chore: adiciona Swagger UI (Swashbuckle) em vez do OpenAPI puro

--- src/Atendimento.Api/Program.cs
+++ src/Atendimento.Api/Program.cs
@@ -9,7 +9,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AtendimentoDbContext>(opcoes =>
     opcoes.UseSqlServer(builder.Configuration.GetConnectionString("AtendimentoDb")));
@@ -27,7 +28,8 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
